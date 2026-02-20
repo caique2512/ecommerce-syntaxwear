c1146a54f89302cfe9d3420df8b645c4f375b55a
Caique Rocha <caiquesantosrocha373@gmail.com>
2026-02-19 23:34:06 -0400

    feat: adiciona header com estilização desjtop

--- js/main.js
+++
@@ -1,8 +0,0 @@
-/* ===================================
-   MAIN.JS - Inicializações globais
-   =================================== */
-
-console.log('SyntaxWear - E-commerce carregado com sucesso');
-
-// Suas inicializações globais aqui
-// Exemplo: inicializar menu, carrinho, etc.
